aa1a39293a2bdf202251f0cc924b6f9f7351b7b8
Dzmitry <dimchikovich1@gmail.com>
2023-09-07 15:09:24 +0200

    commit with VC

---
+++ HWKurylenka_18_BGChange/HWKurylenka_18_BGChange/Controllers/SecondVC.swift
@@ -0,0 +1,28 @@
+//
+//  SecondVC.swift
+//  HWKurylenka_18_BGChange
+//
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
